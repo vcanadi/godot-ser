1122b2baa8b36b7a0cf503cf5ffa3ac13643590c
Vito Canadi <vito.canadi@gmail.com>
2024-12-18 00:43:04 +0100

    Better shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,18 @@
-{ withHLS ? false
+{ withHLS ? false   # Include haskell-language-server in nix-shell
+, forSelf ? withHLS # Include the package itself in the development shell
+, forTests ? withHLS  # Include test dependencies in nix-shell (for working on *Spec files)
+, forExecs ? withHLS
+, forLibs ? withHLS
 , pkgs ? import ./nixpkgs.nix
 }:
 let
   u = import ./util.nix {};
   this = import ./default.nix {};
   ghc = pkgs.haskellPackages.ghcWithPackages
-          (ps:  this.getCabalDeps.libraryHaskellDepends
+          (ps:  (if forSelf then [this] else [])
+             ++ (if forTests then this.getCabalDeps.testHaskellDepends       else [])
+             ++ (if forExecs then this.getCabalDeps.executableHaskellDepends else [])
+             ++ (if forLibs  then this.getCabalDeps.libraryHaskellDepends    else [])
              ++ u.whenFlag withHLS ps.haskell-language-server
           );
 in
@@ -13,3 +20,4 @@ pkgs.stdenv.mkDerivation {
   name = "local-shell-" + (builtins.baseNameOf ./.)  ;
   buildInputs = [ ghc pkgs.haskellPackages.ghcid ];
 }
+
